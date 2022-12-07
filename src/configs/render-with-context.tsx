d25d77f9edd77262cf9b14b6f1b1fb2ea6663129
Dockal <jamesdockal@gmail.com>
2022-12-07 16:50:21 -0300

    feat: create custom render to tests

---
+++ src/configs/render-with-context.tsx
@@ -0,0 +1,15 @@
+import React, { FC, ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react-native';
+import { ThemeProvider } from 'styled-components';
+import { darkTheme } from '../styles/theme';
+
+const AllTheProviders: FC<{ children: React.ReactNode }> = ({ children }) => {
+	return <ThemeProvider theme={darkTheme}>{children}</ThemeProvider>;
+};
+
+export const renderWithContext: any = (
+	ui: ReactElement,
+	options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: AllTheProviders, ...options });
+
+export * from '@testing-library/react-native';
